83a5af7dac52401dd020c1b73d82a25d4547a314
Xblader <gloryofmaster90@gmail.com>
2025-12-30 11:39:35 +0700

    perubahan escpo

--- utils/print.js
+++ utils/print.js
@@ -1,20 +1,20 @@
-// utils/print.js
-import escpos from "escpos";
-escpos.USB = require("escpos-usb");
+// // utils/print.js
+// import escpos from "escpos";
+// escpos.USB = require("escpos-usb");
 
-export function getPrinter() {
-  const devices = escpos.USB.find(); // Deteksi semua printer USB
-  if (devices && devices.length > 0) {
-    return new escpos.USB(devices[0]); // Ambil printer pertama
-  }
-  throw new Error("Printer tidak ditemukan");
-}
+// export function getPrinter() {
+//   const devices = escpos.USB.find(); // Deteksi semua printer USB
+//   if (devices && devices.length > 0) {
+//     return new escpos.USB(devices[0]); // Ambil printer pertama
+//   }
+//   throw new Error("Printer tidak ditemukan");
+// }
 
-export function printOrder(order) {
-  const device = getPrinter();
-  const printer = new escpos.Printer(device);
-  device.open(() => {
-    // ... perintah cetak
-    printer.cut().close();
-  });
-}
+// export function printOrder(order) {
+//   const device = getPrinter();
+//   const printer = new escpos.Printer(device);
+//   device.open(() => {
+//     // ... perintah cetak
+//     printer.cut().close();
+//   });
+// }
