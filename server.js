a322f41f96e7df753fbbc1b8bd143bcbf8329950
Xblader <gloryofmaster90@gmail.com>
2026-01-01 22:37:32 +0700

    production

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: true })); // ← BARIS BARU
 app.use(
   cors({
     origin: ["https://sotoibuksenopati.online", "https://api.sotoibuksenopati.online"],
+    credentials: true,
   })
 );
 
@@ -48,6 +49,6 @@ app.get("/", (req, res) => {
 /* ================= SERVER ================= */
 
 const PORT = 4000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
