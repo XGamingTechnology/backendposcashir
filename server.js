63cf6f941b98d433e043321aa654f9201a8ba3f4
Xblader <gloryofmaster90@gmail.com>
2026-01-01 22:54:56 +0700

    data port

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.get("/", (req, res) => {
 
 const PORT = 4000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
