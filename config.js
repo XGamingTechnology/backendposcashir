fbefe1cde36ba475334764c3ad724b2daa3c4b95
Xblader <gloryofmaster90@gmail.com>
2026-01-02 09:00:38 +0700

    merubah config.js

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
-import pkg from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const { Pool } = pkg;
-
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
